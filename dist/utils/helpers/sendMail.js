83c0ca9ef697a5857c36e5e32ff7dd30e64dd09c
Divyeshrzeusint <divyeshr@zeusint.com>
2025-12-30 09:11:37 +0530

    create new build

--- dist/utils/helpers/sendMail.js
+++ dist/utils/helpers/sendMail.js
@@ -6,26 +6,24 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const nodemailer_1 = __importDefault(require("nodemailer"));
 const sendMail = async (to, subject, htmlTemplate, imageUrl) => {
     const transporter = nodemailer_1.default.createTransport({
-        service: 'gmail',
+        host: 'mail.privateemail.com',
+        port: 587, // TLS
+        secure: false, // false for 587, true for 465
         auth: {
-            user: 'Biblestudykitapp@gmail.com',
-            pass: 'jtlo jveb dilx fhlx'
-        },
-        tls: {
-            // Disable TLS certificate validation
-            rejectUnauthorized: false
+            user: 'team@churchgps.com',
+            pass: 'BibleApp1234' // mailbox or app password
         }
     });
     const mailOptions = {
-        from: 'Biblestudykitapp@gmail.com',
+        from: '"Church GPS" <team@churchgps.com>',
         to,
         subject,
         html: htmlTemplate
     };
-    // Add attachment only if imagePath is provided
     if (imageUrl) {
         mailOptions.attachments = [
             {
+                filename: 'image.jpg',
                 path: imageUrl
             }
         ];
