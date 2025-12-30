1b7d692a830b20c87e2619d2f89ada188d079e8e
Divyeshrzeusint <divyeshr@zeusint.com>
2025-12-30 15:18:48 +0530

    create new build

--- dist/controllers/community.controller.js
+++ dist/controllers/community.controller.js
@@ -4,6 +4,7 @@ exports.getCommunity = exports.createCommunity = void 0;
 const http_status_codes_1 = require("http-status-codes");
 const community_service_1 = require("../services/community.service");
 const general_1 = require("../utils/helpers/general");
+const user_service_1 = require("../services/user.service");
 const createCommunity = async (req, res) => {
     try {
         const userId = req?.user?.userId;
@@ -25,13 +26,16 @@ const getCommunity = async (req, res) => {
         const { lat, lng } = req?.query;
         const data = await (0, community_service_1.getCommunityData)();
         const MAX_DISTANCE = 250;
-        const nearbyCommunities = data?.map((item) => {
+        const allCommunities = await Promise.all(data?.map(async (item) => {
             const distance = (0, general_1.getDistanceMiles)(Number(lat), Number(lng), item?.coordinates?.latitude ?? 0, item?.coordinates?.longitude ?? 0);
+            const userData = await (0, user_service_1.getUserById)(item.userId);
             return {
                 ...item.toObject(),
-                distance: Number(distance.toFixed(2)) // ✅ only number
+                distance: Number(distance.toFixed(2)), // ✅ only number
+                userData
             };
-        }).filter((item) => item.distance <= MAX_DISTANCE);
+        }));
+        const nearbyCommunities = allCommunities.filter((item) => item.distance <= MAX_DISTANCE);
         res.status(http_status_codes_1.StatusCodes.OK).json({ success: true, data: nearbyCommunities, message: 'Communities fetched successfully.' });
     }
     catch (error) {
